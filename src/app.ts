dc0e309e42f71ecc6e615612de230fa8219e92b0
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-09-26 22:15:06 +0800

    current

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,39 @@
 import "dotenv/config";
 import express, { type Express, type Request, type Response, type NextFunction} from 'express';
 import CustomNotFoundError from "../errors/CustomNotFoundError.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
 
 const app: Express = express();
 
 import authorRouter from '../routes/authorRouter.js'
 import bookRouter from '../routes/bookRouter.js'
 import indexRouter from '../routes/indexRouter.js'
+
+const assetsPath = path.join(__dirname,"..","public");
+app.use(express.static(assetsPath));
+
+
+// app.js
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req: Request, res: Response) => {
+  res.render("about", { aboutContent: 'burat na malaki' });
+});
 
 app.use("/authors", authorRouter)
 app.use("/books", bookRouter);
